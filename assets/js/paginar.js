02a62999c6ba142112651cca70c153cc8c022e3f
Agnaldo <aglealjrju@gmail.com>
2023-08-15 22:22:26 -0300

    Projeto1

---
+++ assets/js/paginar.js
@@ -0,0 +1,7 @@
+function paginar(selectedPageId){
+ const paginar = document.getElementById('pag1');
+   
+   paginationLinks.forEach(link => link.classList.remove('active'));
+   link.classList.add('active');
+
+}
